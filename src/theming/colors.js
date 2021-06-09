4d80904dc0cedc14a2f473bab3e8dd2a99f777d3
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-06-09 07:17:15 -0400

    manage colors
    - remove CSS vars in global.css
    - add colors to colors.js (used in compile-time and theme-switching)

--- src/theming/colors.js
+++ src/theming/colors.js
@@ -2,9 +2,15 @@ export default {
   light: {
     text: 'black',
     background: 'white',
+    primary: '#1d048d',
+    secondary: '#1ca086',
+    'subtle-background': '#e6e6e6bb',
   },
   dark: {
     text: 'white',
     background: 'black',
+    primary: '#1ca086',
+    secondary: '#ffdf00',
+    'subtle-background': '#333333',
   },
 }
