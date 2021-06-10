6beb2fb77c77a36a71d91e771d1fb7aa0c0d8bd3
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-06-10 07:36:18 -0400

    content updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,12 @@ export default function About() {
       <div>
         <ul>
           <li>I like going to high places that has a nice view</li>
+          <li>I believe that kindness is more powerful than skill</li>
+          <li>I believe that self-care is essential to progress</li>
+          <li>
+            I used to be a technician, repairing PCs and installing routers
+          </li>
+          <li>I switched from electronics to networks to web dev</li>
           <li>I like making lists...</li>
           <li>...and forgetting about them</li>
         </ul>
